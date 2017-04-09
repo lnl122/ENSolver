408ffa898d31a4d0423aec94454439d1f2bcae7b
Антон <@LNL>
2017-04-10 04:13:12 +0600

    RegistryTest added

---
+++ UnitTestENSolver/RegistryTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using ENSolver;
+
+namespace UnitTestENSolver
+{
+    //[TestClass]
+    public class RegistryTests
+    {
+        [TestMethod]
+        public void Registry_SetGet_1()
+        {
+            Registry reg = new Registry();
+            UserInfo ui = new UserInfo("username1", "password1");
+            reg.SetUserInfo(ui);
+            UserInfo res_ui = reg.GetUserInfo();
+            Assert.AreEqual("username1", res_ui.name);
+            Assert.AreEqual("password1", res_ui.pass);
+        }
+        [TestMethod]
+        public void Registry_SetGet_2()
+        {
+            Registry reg = new Registry(new UserInfo("username2", "password2"));
+            Assert.AreEqual("username2", reg.GetUserInfo().name);
+            Assert.AreEqual("password2", reg.GetUserInfo().pass);
+        }
+    }
+}
